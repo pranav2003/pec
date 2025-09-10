61f16fdff5e896507ed30b8134dff6357d8e22b6
Pranav Kartha <pranav.r.kartha@gmail.com>
2025-09-09 20:41:18 -0500

    added proxies for remote sudo through ssh in tramp setup

--- tramp-setup.el
+++ tramp-setup.el
@@ -1,1 +1,10 @@
+;;;  -*- lexical-binding: t; -*-
+;;; package-setup.el -- Package management
+;;;
+
 (setq vterm-tramp-shells '((t login-shell) ("docker" "/bin/sh")))
+
+;; Add ssh as proxy for sudo for the specified hostnames
+(dolist (hostname '("sisyphus" "lightsail"))
+  (add-to-list 'tramp-default-proxies-alist
+               `(,(concat "\\`" hostname "\\'") "\\`root\\'" "/sshx:%h:")))
