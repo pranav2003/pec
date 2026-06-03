8deb25f7946a422452ea6199a2604ce8d5cbb125
Eric Frederickson <ericfrederickson68@gmail.com>
2026-06-03 17:43:25 -0500

    package-setup: Get rid of the 'vc-use-package' package
    This package provides a ":vc" keyword for 'use-package' declarations,
    which is useful, but: 'use-package' already has its own 'vc' keyword
    built-in on recent Emacs versions, which works differently from that
    provided by the 'vc-use-package' package, and the conflict is
    simply confusing and annoying. So, get rid 'vc-use-package' in favor
    of the built-in default.

--- package-setup.el
+++ package-setup.el
@@ -15,7 +15,3 @@
 (package-initialize)
 
 (setq use-package-always-ensure t)
-
-(unless (package-installed-p 'vc-use-package)
-  (package-vc-install "https://github.com/slotThe/vc-use-package"))
-(require 'vc-use-package)
