16dadee22df448d659be5c15a7c21a1efb574ca9
Eric Frederickson <ericfrederickson68@gmail.com>
2026-06-03 17:51:08 -0500

    calendar-setup: Add a keybind to yank the current date
    The date is yanked in the standard "MM-DD-YYYY" form.

---
+++ calendar-setup.el
@@ -0,0 +1,18 @@
+;;;  -*- lexical-binding: t; -*-
+;;; calendar-setup.el -- Configurations for the built-in Calendar
+;;;
+
+(use-package calendar
+  :bind (:map calendar-mode-map
+              ("y" . calendar-yank-date))
+  :config
+  (defun calendar-yank-date ()
+    "Copy the date at point in mm-dd-yyyy format to the kill ring."
+    (interactive)
+    (let* ((date (calendar-cursor-to-date t))
+           (month (calendar-extract-month date))
+           (day   (calendar-extract-day date))
+           (year  (calendar-extract-year date))
+           (str   (format "%02d-%02d-%04d" month day year)))
+      (kill-new str)
+      (message "Yanked: %s" str))))
--- init.el
+++ init.el
@@ -56,6 +56,9 @@
 	      ;; * Email
 	      "mail-setup.el"
 
+	      ;; * Calendar
+	      "calendar-setup.el"
+
 	      ;; * Terminal
 	      "terminal-setup.el"
 
