6fb2d3b39744f49fb999f30cfdc26c2f15d6f404
Eric Frederickson <ericfrederickson68@gmail.com>
2026-06-19 22:52:45 -0500

    windows-setup: Enhance "C-z k" with a "quickdraw" mode
    Build on the idea of the "C-z k" binding by adding a "quickdraw"
    repeat map wherein one can rapidly blast away buffers and windows that
    they want to get rid of.
    
    In short, typing "C-z k" will now trigger a keymap wherein you can
    repeatedly type "k" to get rid of the next window and buffer. Other
    keys exist in this map as well, that (for example) kill window's
    buffer without deleting the window itself.

--- windows-setup.el
+++ windows-setup.el
@@ -34,6 +34,62 @@
   (keybinds "C-z C-w" ace-window
             "C-z M-w" ace-swap-window))
 
-;; Easy "kill buffer and window" binding
-(keybind "C-z k" (==> (kill-buffer)
-		      (delete-window)))
+;; Quickdraw
+;; ---------
+;; Clear out unwanted buffers and windows with repeatable keys
+
+(defun multiple-windows-in-frame-p ()
+  "Return `t' if the selected frame contains multiple windows,
+and `nil' if it contains only 1 window"
+  (listp (car (window-tree))))
+
+(defun quickdraw/kill-buffer-and-delete-window-dwim ()
+  "Kill the currently-selected buffer and, if the window that displayed
+that buffer is one of multiple windows in the frame, then delete that
+window."
+  (interactive)
+  (kill-buffer)
+  (if (multiple-windows-in-frame-p)
+      (delete-window)))
+
+(defun quickdraw/kill-current-buffer ()
+  "Kill the currently-selected buffer. Don't prompt the user
+(unlike \"M-x kill-buffer\")."
+  (interactive)
+  (kill-buffer))
+
+;; The following commands are all aliases, which are needed so that we
+;; can put them in the `quickdraw-repeat-map' without affecting the
+;; underlying functions they're aliasing to.
+
+(defun quickdraw/delete-window ()
+  "Alias for \"(delete-window)\"."
+  (interactive)
+  (delete-window))
+
+(defun quickdraw/delete-other-windows ()
+  "Alias for \"(delete-other-windows)\"."
+  (interactive)
+  (delete-other-windows))
+
+(defun quickdraw/other-window ()
+  "Alias for \"(other-window 1)\"."
+  (interactive)
+  (other-window 1))
+
+(defun quickdraw/other-window-backwards ()
+  "Alias for \"(other-window -1)\"."
+  (interactive)
+  (other-window -1))
+
+(defvar-keymap quickdraw-repeat-map
+  :repeat t
+  "k" #'quickdraw/kill-buffer-and-delete-window-dwim
+  "K" #'quickdraw/kill-current-buffer
+  "d" #'quickdraw/delete-window
+  "D" #'quickdraw/delete-other-windows
+  "o" #'quickdraw/other-window
+  "O" #'quickdraw/other-window-backwards)
+
+(keybinds "C-z k" quickdraw/kill-buffer-and-delete-window-dwim
+	  "C-z o" quickdraw/other-window)
